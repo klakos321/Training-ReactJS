5b7f4d85ffd1b3435a5913e9a1206d462cbdf0d8
klakos321 <107480814+klakos321@users.noreply.github.com>
2022-06-17 10:37:05 +0300

    Project - Password generator - Basic requirements completed

---
+++ React Training DVSE/Password generator/index.js
@@ -0,0 +1,24 @@
+const characters = [
+    "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+    "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"
+];
+//characters.length == 91
+
+let passwordLen = 15
+
+function generatePassword() {
+    let password1 = ""
+    let password2 = ""
+    for(let i = 0; i < passwordLen*2; i++) {
+        let newCharacter = Math.floor(Math.random() * characters.length)
+        if(i%2 == 0){
+            password1 += characters[newCharacter]
+        } else {
+            password2 += characters[newCharacter]
+        }
+    }
+    document.getElementById("box1").textContent = password1
+    document.getElementById("box2").textContent = password2
+}
